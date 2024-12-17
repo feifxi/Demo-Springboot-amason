9ae36cb6aa83eaf059f4c17ad988c03c87393bb8
Chanombude <137422903+feifxi@users.noreply.github.com>
2024-12-17 19:11:36 +0700

    Update Project125Application.java

--- src/main/java/suchai125/amazon/project125/Project125Application.java
+++ src/main/java/suchai125/amazon/project125/Project125Application.java
@@ -8,7 +8,7 @@ public class Project125Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project125Application.class, args);
-		System.out.println("Server is running!"); // test hellO!
+		System.out.println("Server is running!"); // test hellO! 10
 	}
 
 }
